7d85844babefe46076b203ce3d6a0503fe4c2712
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2021-10-01 17:47:18 -0300

    Initial config redux

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
+import { 
+  MdRemoveCircleOutline, 
+  MdAddCircleOutline, 
+  MdDelete } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  ProductTable, 
+  Total } from './styles';
 
 function Cart() {
   return (
-  <div>
-    <h1>Carrinho</h1>
-  </div>
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <img 
+                  src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280"
+                  alt="Tênis"
+                />
+              </td>
+              <td>
+                <strong>Tênis muito massa</strong>
+                <span>R$134,20</span>
+              </td>
+              <td>
+                <div>
+                  <button type="button">
+                    <MdRemoveCircleOutline size={20} color="7159c1" />
+                  </button>
+                  <input type="number" readOnly value={1} />
+                  <button type="button">
+                    <MdAddCircleOutline size={20} color="7159c1" />
+                  </button>
+                </div>
+              </td>
+              <td>
+                <strong>R$400,00</strong>
+              </td>
+              <td>
+                <button type="button">
+                  <MdDelete size={20} color="9159c1" />
+                </button>
+              </td>
+            </tr>
+          </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">
+          Finalizar pedido
+        </button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$139,90</strong>
+        </Total>
+      </footer>
+    </Container>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,106 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProducts() {
+        const response = await api.get('products');
+
+        const data = response.data.map(product => ({
+          ...product,
+          priceFormatted: formatPrice(product.price),
+        }));
+
+        setProducts(data);
+    }
+
+    getProducts();
+  }, []);
+
   return (
     <ProductList>
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
+      { products.map(product => (
+        <li key={product.id}>
+          <img src={product.image}
+              alt={product.title} />
 
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-
-      <li>
-        <img src="https://static.netshoes.com.br/produtos/tenis-adidas-runfalcon-20-masculino/26/3ZP-0573-026/3ZP-0573-026_detalhe1.jpg?ts=1626193145?ims=280x280" 
-             alt="Tênis" />
-
-        <strong>Tênis muito legal</strong>
-        <span>R$139,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      )) }
     </ProductList>
   );
 }
