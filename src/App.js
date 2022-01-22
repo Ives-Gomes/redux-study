1bd3ac31fdfeb82cae8f1b9827a691f4aaed3ee8
Ives Moreira Gomes <ives.gomes@acensjr.com>
2022-01-22 01:06:25 -0300

    Reactotron config

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Routes from './routes';
+import './config/ReactotronConfig';
+
 import Header from './components/Header';
+
+import Routes from './routes';
 import store from './store';
 
 import GlobalStyle from './styles/global';
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { connect, useDispatch } from 'react-redux';
 
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -6,35 +7,47 @@ import api from '../../services/api';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
-export default function Home() {
+const Home = () => {
+  const dispatch = useDispatch();
+
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     async function getProducts() {
-        const response = await api.get('products');
+      const response = await api.get('products');
 
-        const data = response.data.map(product => ({
-          ...product,
-          priceFormatted: formatPrice(product.price),
-        }));
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: formatPrice(product.price),
+      }));
 
-        setProducts(data);
+      setProducts(data);
     }
 
     getProducts();
   }, []);
 
+  const handleAddProduct = (product) => {
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  }
+
   return (
     <ProductList>
-      { products.map(product => (
+      {products.map(product => (
         <li key={product.id}>
           <img src={product.image}
-              alt={product.title} />
+            alt={product.title} />
 
           <strong>{product.title}</strong>
           <span>{product.priceFormatted}</span>
 
-          <button type="button">
+          <button
+            type="button"
+            onClick={() => handleAddProduct(product)}
+          >
             <div>
               <MdAddShoppingCart size={16} color="#FFF" /> 3
             </div>
@@ -42,7 +55,9 @@ export default function Home() {
             <span>ADICIONAR AO CARRINHO</span>
           </button>
         </li>
-      )) }
+      ))}
     </ProductList>
   );
-}
+};
+
+export default connect()(Home);
